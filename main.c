6b8384b4bde7e20343e96961ffae694e63b52413
Aymane Mehdi <unlocked.dark.web@gmail.com>
2023-08-21 14:38:33 +0100

    printf

--- main.c
+++ main.c
@@ -4,24 +4,21 @@
 
 int main(void)
 {
-int len;
-unsigned int ui;
+    int len;
+    int len2;
+    unsigned int ui;
+    void *addr;
 
-len = _printf("Let's try to printf a simple sentence.\n");
-_printf("Length:[%d, %i]\n", len, len);
-_printf("Unknown:[%r]\n");
-len = _printf("Percent:[%%]\n");
-_printf("String:[%s]\n", "I am a string !");
+    len = _printf("Let's try to printf a simple sentence.\n");
+    _printf("Length:[%d, %i]\n", len, len);
+    _printf("Unknown:[%r]\n");
+    len = _printf("Percent:[%%]\n");
+    _printf("String:[%s]\n", "I am a string !");
 
+    ui = (unsigned int)INT_MAX + 1024;
+    _printf("Unsigned:[%u]\n", ui);
+    printf("Unsigned:[%u]\n", ui);
 
-ui = (unsigned int)INT_MAX + 1024;
-_printf("Unsigned:[%u]\n", ui);
-printf("Unsigned:[%u]\n", ui);
-_printf("Unsigned octal:[%o]\n", ui);
-printf("Unsigned octal:[%o]\n", ui);
-_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-
-/* printf("%d\n",len); */
-return (0);
+    /* printf("%d\n",len); */
+    return (0);
 }
