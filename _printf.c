8595fd564aacc6050ec9cee8c3b3567e28b3193a
Aymane Mehdi <unlocked.dark.web@gmail.com>
2023-08-21 07:12:57 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,30 +1,74 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
- * _printf - Produces output according to a format
- * @format: Is a character string. The format string
- * is composed of zero or more directives
- *
- * Return: The number of characters printed (excluding
- * the null byte used to end output to strings)
- **/
-int _printf(const char *format, ...)
-{
-	int size;
-	va_list args;
+ * Placeholder functions for printing various data types.
+ * You need to implement these functions according to the data types.
+ */
+void print_str(va_list pa, int *pCount) {
+    // Implement print_str function here
+}
 
-	if (format == NULL)
-		return (-1);
+void print_ch(va_list pa, int *pCount) {
+    // Implement print_ch function here
+}
 
-	size = _strlen(format);
-	if (size <= 0)
-		return (0);
+// Define other print functions for different data types
 
-	va_start(args, format);
-	size = handler(format, args);
+// Structure to map special characters to their corresponding print functions
+struct sp_char {
+    char ch;
+    void (*fun)(va_list, int *);
+};
 
-	_putchar(-1);
-	va_end(args);
+// Define the print functions for special characters
+struct sp_char type[] = {
+    {'s', print_str}, {'c', print_ch}, /* ... define for other data types */
+    {'\0', NULL}
+};
 
-	return (size);
+int spIndex(char ch, const struct sp_char *p) {
+    for (int i = 0; p[i].ch != '\0'; i++) {
+        if (p[i].ch == ch) {
+            return i;
+        }
+    }
+    return -1; // Character not found
+}
+
+void call_sp(const char *format, int *i, struct sp_char *p, int *pCount, va_list pa) {
+    // Implement the flag handling and function calling logic here
+}
+
+int _printf(const char *format, ...) {
+    va_list pa;
+    int i, count = 0;
+    int *pCount = &count;
+
+    if (format == NULL || (format[0] == '%' && format[1] == '\0')) {
+        return 0; // Return 0 to indicate no characters were printed
+    }
+
+    va_start(pa, format);
+    for (i = 0; format[i] != '\0'; i++) {
+        if (format[i] != '%') {
+            putchar(format[i]);
+            (*pCount)++;
+        } else if (format[i] == '%' && format[i + 1] != '%') {
+            i++;
+            call_sp(format, &i, type, pCount, pa);
+        } else if (format[i] == '%' && format[i + 1] == '%') {
+            i++;
+            putchar('%');
+            (*pCount)++;
+        }
+    }
+    va_end(pa);
+    return count;
+}
+
+int main() {
+    int result = _printf("String: %s, Character: %c, Integer: %d\n", "Hello", 'A', 42);
+    printf("Number of characters printed: %d\n", result);
+    return 0;
 }
